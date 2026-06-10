d771a21baa9d89037d1f24e1199706969bd0021a
Rapthan <151851356+Rapthan@users.noreply.github.com>
2026-06-10 15:17:07 +0200

    Broker
    Broker is done and docker-compose updated to add the broker

---
+++ ReseauTP1/game_sockets/src/protocols/mod.rs
@@ -0,0 +1,12 @@
+#[cfg(feature = "udp")]
+mod udp_protocol;
+#[cfg(feature = "udp")]
+pub use udp_protocol::UdpBackend;
+#[cfg(feature = "tcp")]
+mod tcp_protocol;
+#[cfg(feature = "tcp")]
+pub use tcp_protocol::TcpBackend;
+#[cfg(feature = "quic")]
+mod quic_protocol;
+#[cfg(feature = "quic")]
+pub use quic_protocol::QuicBackend;
